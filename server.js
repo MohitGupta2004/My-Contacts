1222d6d3fe5542c89686596f5a0a81127e892399
MohitGupta2004 <mohit0907gupta@gmail.com>
2025-04-26 01:04:48 +0530

    added default get

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
